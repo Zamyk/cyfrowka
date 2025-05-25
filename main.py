39af6f0a38dafe2b8348f2274bd9edbd02fc87be
Jan Chadzinski <108220964+Zamyk@users.noreply.github.com>
2025-05-25 02:16:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from automata.fa.dfa import DFA
 from visual_automata.fa.dfa import VisualDFA
+import logicmin
 
-states = ["000", "001", "010", "011", "100", "101", "110", "111"]
-
-# state: playing(1 bit) song_number(2 bits)
+states = ["0000", "0100", "0001", "0101", "0010", "0110", "0011", "0111", "1000", "1100", "1001", "1101", "1010", "1110", "1011", "1111"]
+# state: playing(1 bit) pressed(1 bit) song_number(2 bits)
 
 possible_inputs = [format(i, '04b') for i in range(2 ** 4)]
 # input: stop, play, next, previous
@@ -33,23 +33,34 @@ def process_input(input):
 
 def transition(state, processed):
   playing = state[0]
-  number = state[1:3]
-  # stop
-  if processed[0] == "1":
-    playing = "0"
+  pressed = state[1]
+  number = state[2:4]
 
-  # play
-  if processed[1] == "1":
-    playing = "1"
+  if pressed == "0":
+    # stop
+    if processed[0] == "1":
+      playing = "0"
+      pressed = "1"
 
-  # next
-  if processed[2] == "1":
-    number = move(number, 1)
+    # play
+    if processed[1] == "1":
+      playing = "1"
+      pressed = "1"
 
-  if processed[3] == "1":
-    number = move(number, -1)
+    # next
+    if processed[2] == "1":
+      number = move(number, 1)
+      pressed = "1"
 
-  return playing + number
+    if processed[3] == "1":
+      number = move(number, -1)
+      pressed = "1"
+
+  else:
+    if input == "0000":
+      pressed = "0"
+
+  return playing + pressed + number
 
 def get_transitions(state):
   ans = dict()
@@ -63,7 +74,7 @@ def draw_dfa():
     transitions = {
         state: get_transitions(state) for state in states
     }
-    
+
     # Extract input symbols directly from the keys of the transitions
     input_symbols = set(trans for trans in transitions.values() for trans in trans.keys())
 
@@ -71,14 +82,27 @@ def draw_dfa():
         states=set(states),
         input_symbols=input_symbols,
         transitions=transitions,
-        initial_state="000",
+        initial_state="0000",
         final_states=set(),  # Define final states if any
-    )    
-    
+    )
     print(dfa.table)
 
 def get_logic_functions():
-  pass # todo, use minimize thing to create the logic functions
+  tt = logicmin.TT(8, 4)
+  for state in states:
+    for input in possible_inputs:
+      processed = process_input(input)
+      t = transition(state, processed)
+      tt.add(state + input, t)
 
-# Draw the DFA
+  sols = tt.solve()
+  print(sols.printN(xnames=["playing", "pressed", "numbe1", "number2", "stop", "play", "next", "previous"],ynames=["out_playing", "out_pressed", "out_number1", "out_number2"], info=True))  
+
 draw_dfa()
+
+print()
+print()
+print()
+print()
+
+get_logic_functions()
